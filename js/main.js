0c0fcdf71a76d2047973ea9253394eef2dd81e33
Muhammad Shahzad Bhatti <muhammad.2941906@talmeez.pk>
2021-01-11 22:43:44 +0500

    Completed

---
+++ js/main.js
@@ -0,0 +1,46 @@
+const search = document.querySelector("#search");
+const matchList = document.querySelector("#match-list");
+
+// Search states.json and filter it
+const searchStates = async (searchText) => {
+  const res = await fetch("../data/states.json");
+  const states = await res.json();
+
+  // Get matches to current text input
+  let matches = states.filter((state) => {
+    matchList.innerHTML = "";
+    const regex = new RegExp(`^${searchText}`, "gi");
+    return state.name.match(regex) || state.abbr.match(regex);
+  });
+
+  if (searchText.length === 0) {
+    matches = [];
+    matchList.innerHTML = "";
+  }
+
+  outputHtml(matches);
+};
+
+// Show results in HTML
+const outputHtml = (matches) => {
+  if (matches.length > 0) {
+    const html = matches
+      .map(
+        (match) => `
+    <div class="card card-body mb-1">
+        <h4>
+        ${match.name} (${match.abbr}) 
+            <span class="text-primary">
+            ${match.capital}
+            </span>
+        </h4>
+        <small>Lat: ${match.lat} / Lng: ${match.long}</small>
+    </div>
+    `
+      )
+      .join("");
+    matchList.innerHTML = html;
+  }
+};
+
+search.addEventListener("input", () => searchStates(search.value));
